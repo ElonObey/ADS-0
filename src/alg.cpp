da6dae209fe4aabc0ce9edda6e47a4030d530879
ElonObey <gulin1999@mail.ru>
2021-03-09 11:59:24 +0300

    Dmitriy Gulin
    Выполненая работа по "ADS-0"
    Выполнил студент группы 20-ИСТ-2
    Гулин Дмитрий Михайлович

--- src/alg.cpp
+++ src/alg.cpp
@@ -5,12 +5,11 @@
 //
 
 int gcd(int a, int b) {
-   while (a != b)
-	{
-		if (a > b)
-			a = a - b;
-
-		else b = b - a;
-	}
+while (a != b)
+{
+	if (a > b)
+		a = a - b;
+	else b = b - a;
+}
 	return a;
 }

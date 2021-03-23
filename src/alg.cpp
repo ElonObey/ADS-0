6dcb2600ec4d51c281db6932d5d4082d1e78f82a
ElonObey <gulin1999@mail.ru>
2021-03-23 14:34:26 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,14 +6,13 @@
 
 int gcd(int a, int b) {
 while (a != b)
-{
-	if ( a > b)
-	{
-		a = a - b;
-	} else
-	{
-		b = b - a;
-	}
+{if ( a > b)
+ {
+  a = a - b;
+  } else
+ {
+  b = b - a;
+ }
 }
-	return a;
+ return a;
 }
